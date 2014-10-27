303bc0550a0d98485e23c3cf633cd854258adb85
Konstantin Artemiev <kartemiev@gmail.com>
2014-10-27 22:58:25 +0700

    agi: сделан тест для фабрики Dialout Controller

---
+++ agi/module/PbxAgi/test/PbxAgiTest/Controller/DialOut/DialoutControllerFactoryTest.php
@@ -0,0 +1,25 @@
+<?php
+namespace PbxAgiTest\Controller\DialOut;
+use \PbxAgiTest\Bootstrap;
+use PHPUnit_Framework_TestCase;
+use PbxAgi\Controller\DialOutControllerFactory;
+
+
+class DialOutControllerFactoryTest extends PHPUnit_Framework_TestCase
+{
+	protected $dialOutController;
+    public function setUp()
+    {
+        $serviceManager = Bootstrap::getServiceManager();         
+        $serviceManager->setAllowOverride(true);         
+         
+        $factory = new DialOutControllerFactory();
+                
+        $this->dialOutController = $factory->createService($serviceManager);        
+    }
+    public function testFactoryReturnsInstanceofDialOutController()
+    {
+    	$this->setUp();
+        $this->assertInstanceOf('PbxAgi\Controller\DialOutController', $this->dialOutController);
+    }
+}
